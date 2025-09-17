9f5e67e9bc3ebe77dd120b8e80cf7972f6b42106
Tyson Hu <me@tianzhe.me>
2025-09-17 13:36:31 -0500

    init commit

---
+++ Food Scanner/Food_ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_ScannerApp.swift
+//  Food Scanner
+//
+//  Created by Tyson Hu on 9/17/25.
+//
+
+import SwiftUI
+
+@main
+struct Food_ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
